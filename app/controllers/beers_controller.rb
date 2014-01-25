e14e139b95d72aced50138e12be764a2d2fa17a4
Henri Korpela <henri.korpela@hotmail.com>
2014-01-25 13:54:52 +0200

    Toinen korjaus yritys

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -35,6 +35,8 @@ class BeersController < ApplicationController
   # GET /beers/1/edit
   def edit
     @beer = Beer.find(params[:id])
+     @breweries = Brewery.all 
+     @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
   end
 
   # POST /beers
