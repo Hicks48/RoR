ac78aa7109e2f8b590b638bd1b3a080c83271b0f
Henri Korpela <henri.korpela@hotmail.com>
2014-02-01 14:48:15 +0200

    Kolmas viikko korjauksia

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,19 +18,19 @@ b2.beers.create name:"X Porter", style:"Porter"
 b3.beers.create name:"Hefezeizen", style:"Weizen"
 b3.beers.create name:"Helles", style:"Lager"
 
-user1 = User.create(username:"user_1", password:"User123")
-user2 = User.create(username:"user_2", password:"User456")
+user1 = User.create username:"user_1", password:"User123"
+user2 = User.create username:"user_2", password:"User456"
 
 ber1 = Beer.find_by_name("Iso 3")
 ber2 = Beer.find_by_name("X Porter")
 ber3 = Beer.find_by_name("Helles")
 ber4 = Beer.find_by_name("Tuplahumala")
 
-ber1.ratings.create(score:10, user_id:user_1.id)
-ber1.ratings.create(score:8, user_id:user_1.id)
-ber2.ratings.create(score:4, user_id:user_1.id)
-ber2.ratings.create(score:7, user_id:user_1.id)
-ber3.ratings.create(score:14, user_id:user_2.id)
-ber3.ratings.create(score:18, user_id:user_2.id)
-ber4.ratings.create(score:5, user_id:user_2.id)
-ber4.ratings.create(score:6, user_id:user_2.id)
+ber1.ratings.create(score:10, user_id:user1.id)
+ber1.ratings.create(score:8, user_id:user1.id)
+ber2.ratings.create(score:4, user_id:user1.id)
+ber2.ratings.create(score:7, user_id:user1.id)
+ber3.ratings.create(score:14, user_id:user2.id)
+ber3.ratings.create(score:18, user_id:user2.id)
+ber4.ratings.create(score:5, user_id:user2.id)
+ber4.ratings.create(score:6, user_id:user2.id)
